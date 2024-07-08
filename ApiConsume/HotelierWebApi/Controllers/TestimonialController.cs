e1b57aca1e4936a41a4ed9ffc2f2bc5e1f194765
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-07-08 14:35:32 +0300

    Api hazirlandi

---
+++ ApiConsume/HotelierWebApi/Controllers/TestimonialController.cs
@@ -0,0 +1,51 @@
+﻿using HotelierBusinessLayer.Abstract;
+using HotelierEntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelierWebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TestimonialController : ControllerBase
+	{
+		private readonly ITestimonialService _testimonialService;
+
+		public TestimonialController(ITestimonialService testimonialService)
+		{
+			_testimonialService = testimonialService;
+		}
+
+		[HttpGet]
+		public IActionResult TestimonialList()
+		{
+			var values = _testimonialService.TGetList();
+			return Ok(values);
+		}
+		[HttpPost]
+		public IActionResult Addtestimonial(Testimonial testimonial)
+		{
+			_testimonialService.TInsert(testimonial);
+			return Ok();
+		}
+		[HttpDelete]
+		public IActionResult Deletetestimonial(int id)
+		{
+			var values = _testimonialService.TGetById(id);
+			_testimonialService.TDelete(values);
+			return Ok();
+		}
+		[HttpPut]
+		public IActionResult Updatetestimonial(Testimonial testimonial)
+		{
+			_testimonialService.TUpdate(testimonial);
+			return Ok();
+		}
+		[HttpGet("{id}")]
+		public IActionResult Gettestimonial(int id)
+		{
+			var values = _testimonialService.TGetById(id);
+			return Ok(values);
+		}
+	}
+}
--- ApiConsume/HotelierWebApi/Program.cs
+++ ApiConsume/HotelierWebApi/Program.cs
@@ -1,8 +1,32 @@
+using HotelierBusinessLayer.Abstract;
+using HotelierBusinessLayer.Concrete;
+using HotelierDataAccessLayer.Abstract;
+using HotelierDataAccessLayer.Concrete;
+using HotelierDataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<Context>();
+
+builder.Services.AddScoped<IStaffDal, EfStaffDal>();
+builder.Services.AddScoped<IStaffService, StaffManager>();
+
+builder.Services.AddScoped<IServiceDal, EfServiceDal>();
+builder.Services.AddScoped<IServiceService, ServiceManager>();
+
+builder.Services.AddScoped<IRoomDal, EfRoomDal>();
+builder.Services.AddScoped<IRoomService, RoomManager>();
+
+builder.Services.AddScoped<ISubscribeDal, EfSubscribeDal>();
+builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+
+builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
