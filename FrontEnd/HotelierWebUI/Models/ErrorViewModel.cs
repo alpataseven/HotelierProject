56a900c5dc3786d5fd70e3dafa73f631e974c77d
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-07-08 17:08:58 +0300

    Admin paneli eklendi..

---
+++ FrontEnd/HotelierWebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelierWebUI.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
